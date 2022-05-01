1ec327bb30c790c54b69eb351543e222afb2f189
Er3n <erenripit@gmail.com>
2022-05-01 18:16:35 +0300

    Kararlı Sürüm

--- sketch_may01a/sketch_may01a.ino
+++ sketch_may01a/sketch_may01a.ino
@@ -1,37 +1,28 @@
+//ER3N
+
+#include "NewPing.h"
 #include <LiquidCrystal_I2C.h>
+#define ECHO_PIN 7
+#define TRIG_PIN 6
+#define MAX_UZAKLIK 400
 
-#include <Wire.h>
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_UZAKLIK);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-LiquidCrystal_I2C lcdekranim(0x27,16,2);
+float mesafe,boy;
+
 void setup() {
-  lcdekranim.init();
-  lcdekranim.backlight();
-  lcdekranim.setCursor(0,0);
-  lcdekranim.print("|-LOGIN-|");
-  delay(1000);
-  lcdekranim.clear();
-  pinMode(6,OUTPUT);//Trig
-  pinMode(7,INPUT); //Echo
-  pinMode(4,OUTPUT);  
-  Serial.begin(9600);
-
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-digitalWrite(6,1); delay(1);
-digitalWrite(6,0);
-int sure=pulseIn(7,1);
-int mesafe=(sure/2)/28.97;
-int boy=mesafe-200;
-Serial.print("Olculen Mesafe=");
-Serial.println(mesafe);
-delay(1);
-
-lcdekranim.setCursor(0,0);
-lcdekranim.print("|-Boyunuz-|");
-lcdekranim.setCursor(3,1);
-lcdekranim.print(boy);
-delay(0120);
-lcdekranim.clear();
-
+  mesafe = sonar.ping_cm();
+  boy = 200 - mesafe;
+  lcd.setCursor(0,0);
+  lcd.print("|-BOYUNUZ-|");
+  lcd.setCursor(0,1);
+  lcd.print(boy);
+  delay(200);
+  lcd.clear();
 }
